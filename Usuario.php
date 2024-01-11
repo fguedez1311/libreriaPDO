4279fe213612737a75663d4173a130a3d3e12266
fguedez1311 <fguedez1311@gmail.com>
2024-01-11 15:56:14 -0400

    CRUD Completo

--- Usuario.php
+++ Usuario.php
@@ -26,4 +26,33 @@
             $request=$execute->fetchAll(PDO::FETCH_ASSOC);
             return $request;
         }
+        public function updateUser(int $id,string $nombre,int $telefono, string $email){
+            $this->strNombre=$nombre;
+            $this->intTelefono=$telefono;
+            $this->strEmail=$email;
+            $sql="UPDATE usuario SET nombre=?,telefono=?,email=? WHERE id=$id";
+            $update=$this->connect()->prepare($sql);
+            $arrayData=array($this->strNombre, $this->intTelefono,$this->strEmail);
+            $resExecute=$update->execute($arrayData);
+            return $resExecute;
+        }
+        // Extraer un registro de la BD
+
+        public function getUser(int $id){
+            $sql="SELECT * FROM usuario WHERE id=?";
+            $arrayWhere=array($id);
+            $query=$this->connect()->prepare($sql);
+            $query->execute($arrayWhere);
+            $request=$query->fetch(PDO::FETCH_ASSOC);
+            return $request;
+        }
+        // Borrar un registro de la BD
+        
+        public function deluser(int $id){
+            $sql="DELETE FROM usuario WHERE id=?";
+            $arrayWhere=array($id);
+            $delete=$this->connect()->prepare($sql);
+            $del=$delete->execute($arrayWhere);
+            return $del;
+        }
      } 
--- sistema.php
+++ sistema.php
@@ -9,5 +9,14 @@
     print_r("<pre>");
     print_r($users);
     print_r("</pre>");
-    
+    // $updateUser=$objUsuario->updateUser(15,"Felipe Guedez",4249338526,"felipe@gmail.com");
+    // echo $updateUser;
+
+    $user=$objUsuario->getUser(15);
+    print_r("<pre>");
+    print_r($user);
+    print_r("</pre>");
+
+    $delete=$objUsuario->deluser(15);
+    echo $delete;
 ?>
